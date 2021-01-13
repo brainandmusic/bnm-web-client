752105336e71fc9796462da7b742e0645df82387
Wenhe Qi <wenhe.qi@outlook.com>
2021-01-12 21:10:04 -0800

    Add lab.js 20.1.1

---
+++ public/experiments/runner/labjs/study.js
@@ -0,0 +1,13 @@
+/*eslint no-undef: "off"*/
+
+// Define the sequence of components that define the study
+const study = new lab.flow.Sequence({
+  content: [
+    new lab.html.Screen({
+      content: 'The experiment is running!',
+    }),
+  ],
+})
+
+// Start the study
+study.run()
