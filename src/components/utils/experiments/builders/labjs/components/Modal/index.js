1eafdafaf2c021a344c020364542297cd1dd88a1
Wenhe Qi <wenhe.qi@outlook.com>
2021-01-16 14:26:13 -0800

    Fix modal display position issue

--- src/components/utils/experiments/builders/labjs/components/Modal/index.js
+++ src/components/utils/experiments/builders/labjs/components/Modal/index.js
@@ -5,27 +5,34 @@ import classnames from 'classnames'
 // Overall general-purpose modal container style
 import './index.css'
 
-export default ({ isOpen, onRequestClose, modalProps={}, children }) =>
+export default ({ isOpen, onRequestClose, modalProps = {}, children }) =>
   <Modal
-    appElement={ document.getElementById('root') }
-    isOpen={ isOpen }
-    onRequestClose={ onRequestClose }
-    className={ classnames({
+    appElement={document.getElementById('root')}
+    isOpen={isOpen}
+    onRequestClose={onRequestClose}
+    className={classnames({
       'modal-dialog': true,
       'modal-lg': modalProps.size === 'lg',
       'modal-xl': modalProps.size === 'xl',
-    }) }
+    })}
     overlayClassName="modal fade show"
     contentLabel="App Modal"
     style={{
       overlay: {
         display: 'block',
-        backgroundColor : 'rgba(0, 0, 0, 0.5)',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
         opacity: '1',
         overflowX: 'hidden',
         overflowY: 'auto',
-      }
+        position: 'fixed',
+        top: '0',
+        right: '0',
+        bottom: '0',
+        left: '0',
+        // add padding for app bar
+        paddingTop: '70px',
+      },
     }}
   >
-    { children }
+    {children}
   </Modal>
