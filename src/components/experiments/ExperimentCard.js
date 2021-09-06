d51334bdf1317b13fe28ce6224f649e23b9ff167
Chen Qian <cqian550@usc.edu>
2021-09-06 15:13:15 -0700

    push minor fixes

--- src/components/experiments/ExperimentCard.js
+++ src/components/experiments/ExperimentCard.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { useHistory } from 'react-router-dom';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import TuneIcon from '@material-ui/icons/Tune';
-import Typography from '@material-ui/core/Typography';
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: "100%",
+    maxWidth: "100%"
   },
   title: {
-    fontSize: 14,
+    fontSize: 14
   },
   description: {
-    marginBottom: 12,
+    marginBottom: 12
   },
   expend: {
-    marginLeft: "auto",
+    marginLeft: "auto"
   }
 });
 
@@ -34,7 +33,7 @@ function ExperimentCard({ role, name, description, platform, _id }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const menuOpen = Boolean(anchorEl);
 
-  const handleMenuClick = (event) => {
+  const handleMenuClick = event => {
     setAnchorEl(event.currentTarget);
   };
 
@@ -48,80 +47,87 @@ function ExperimentCard({ role, name, description, platform, _id }) {
     // 2) create a modal to show the user progress/status of the running experiment in another window
     // 3) when user completes the experiment, upload response to database
     const windowFeatures = "menubar=no,location=no,toolbar=no,resizable=yes";
-    window.open(`${process.env.PUBLIC_URL}/experiments/runner/${platform.replace(/\./g, "").toLowerCase()}/`, "", windowFeatures);
-  }
+    window.open(
+      `${process.env.PUBLIC_URL}/experiments/runner/${platform
+        .replace(/\./g, "")
+        .toLowerCase()}/`,
+      "",
+      windowFeatures
+    );
+  };
 
   const handleConfigClick = () => {
-    history.push(`/experiments/builder/platform/${platform.replace(/\./g, "").toLowerCase()}/experiment/${_id}`)
-  }
+    history.push(
+      `/experiments/builder/platform/${platform
+        .replace(/\./g, "")
+        .toLowerCase()}/experiment/${_id}`
+    );
+  };
 
   return (
     <Card className={classes.root}>
-      <CardActionArea onClick={role === "admin" ? handleConfigClick : handleRunClick}>
+      <CardActionArea
+        onClick={role === "admin" ? handleConfigClick : handleRunClick}
+      >
         <CardContent>
-          {
-            role === "admin" ? (
-              <Typography variant="subtitle2" color="textSecondary">
-                {platform}
-              </Typography>
-            ) : null
-          }
-          <Typography variant="subtitle1" component="h2" gutterBottom >
+          {role === "admin" ? (
+            <Typography variant="subtitle2" color="textSecondary">
+              {platform}
+            </Typography>
+          ) : null}
+          <Typography variant="subtitle1" component="h2" gutterBottom>
             {name}
           </Typography>
-          <Typography variant="body2" color="textSecondary" className={classes.description}>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            className={classes.description}
+          >
             {description || ""}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions disableSpacing>
-        {
-          role === "admin" ? null : (
-            <IconButton aria-label="start experiment" onClick={handleRunClick}>
-              <PlayArrowIcon />
+        {role === "admin" ? null : (
+          <IconButton aria-label="start experiment" onClick={handleRunClick}>
+            <PlayArrowIcon />
+          </IconButton>
+        )}
+        {role === "admin" ? (
+          <>
+            <IconButton
+              aria-label="more options"
+              aria-controls="expend-menu"
+              aria-haspopup="true"
+              className={classes.expend}
+              onClick={handleMenuClick}
+            >
+              <MoreVertIcon />
             </IconButton>
-          )
-        }
-        {
-          role === "admin" ? (
-            <>
-              <IconButton aria-label="config experiment" onClick={handleConfigClick}>
-                <TuneIcon />
-              </IconButton>
-              <IconButton
-                aria-label="more options"
-                aria-controls="expend-menu"
-                aria-haspopup="true"
-                className={classes.expend}
-                onClick={handleMenuClick}
-              >
-                <MoreVertIcon />
-              </IconButton>
-              <Menu
-                id="expend-menu"
-                anchorEl={anchorEl}
-                getContentAnchorEl={null}
-                keepMounted
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'center',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={menuOpen}
-                onClose={handleMenuClose}
-              >
-                <MenuItem onClick={handleMenuClose} experimentid={_id}>
-                  Delete
-                </MenuItem>
-              </Menu>
-            </>
-          ) : null
-        }
+            <Menu
+              id="expend-menu"
+              anchorEl={anchorEl}
+              getContentAnchorEl={null}
+              keepMounted
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center"
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right"
+              }}
+              open={menuOpen}
+              onClose={handleMenuClose}
+            >
+              <MenuItem onClick={handleMenuClose} experimentid={_id}>
+                Delete
+              </MenuItem>
+            </Menu>
+          </>
+        ) : null}
       </CardActions>
-    </Card >
+    </Card>
   );
 }
 
--- src/components/tables/ExperimentTable.js
+++ src/components/tables/ExperimentTable.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
-import Link from '@material-ui/core/Link';
-import MuiAlert from '@material-ui/lab/Alert';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Snackbar from '@material-ui/core/Snackbar';
-import DeleteModal from '../modals/DeleteModal';
-import ExperimentService from '../../services/Experiment';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+import Link from "@material-ui/core/Link";
+import MuiAlert from "@material-ui/lab/Alert";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Snackbar from "@material-ui/core/Snackbar";
+import DeleteModal from "../modals/DeleteModal";
+import ExperimentService from "../../services/Experiment";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -22,11 +22,20 @@ function Alert(props) {
 const useStyles = makeStyles({
   table: {
     maxWidth: "100%",
-    overflow: "scroll",
-  },
+    overflow: "scroll"
+  }
 });
 
-function ExperimentTable({ expIds = [], delModP1, delModP2, handleDelete, snackOpen, handleSnackClose, snackSev, snackMsg }) {
+function ExperimentTable({
+  expIds = [],
+  delModP1,
+  delModP2,
+  handleDelete,
+  snackOpen,
+  handleSnackClose,
+  snackSev,
+  snackMsg
+}) {
   const classes = useStyles();
   const [loading, setLoading] = useState(true);
   const [experiments, setExperiments] = useState([]);
@@ -36,21 +45,23 @@ function ExperimentTable({ expIds = [], delModP1, delModP2, handleDelete, snackO
   const handleDelModalOpen = () => setDelModalOpen(true);
   const handleDelModalClose = () => setDelModalOpen(false);
 
-  const handleClickDelete = (e) => {
+  const handleClickDelete = e => {
     handleDelModalOpen();
-    setDelUid(e.target.closest("button").getAttribute('expid'));
-  }
+    setDelUid(e.target.closest("button").getAttribute("expid"));
+  };
 
   const handleProcessDelete = () => {
     handleDelModalClose();
     handleDelete && handleDelete(delUid);
-  }
+  };
 
   useEffect(() => {
     async function loadExperiments(expIds) {
-      let results = await Promise.all(expIds.map(async (expId) => {
-        return await ExperimentService.getExperiment(expId, 1);
-      }));
+      let results = await Promise.all(
+        expIds.map(async expId => {
+          return await ExperimentService.getExperiment(expId, 1);
+        })
+      );
       results.forEach(result => {
         if (result.status === "OK") {
           setExperiments(old => [...old, result.result]);
@@ -60,10 +71,10 @@ function ExperimentTable({ expIds = [], delModP1, delModP2, handleDelete, snackO
     setExperiments([]);
     loadExperiments(expIds);
     setLoading(false);
-  }, [expIds])
+  }, [expIds]);
 
   if (loading) {
-    return (<div>Loading ...</div>)
+    return <div>Loading ...</div>;
   }
   return (
     <TableContainer component={Paper}>
@@ -78,18 +89,26 @@ function ExperimentTable({ expIds = [], delModP1, delModP2, handleDelete, snackO
           </TableRow>
         </TableHead>
         <TableBody>
-          {experiments.map((experiment) => (
+          {experiments.map(experiment => (
             <TableRow key={experiment._id}>
               <TableCell component="th" scope="row">
-                <Link href={`/experiments/builder/platform/${experiment.platform.toLowerCase().replaceAll(".", "")}/experiment/${experiment._id}`} color="inherit" underline="none">
-                  {experiment._id}
-                </Link>
+                {/* <Link href={`/experiments/builder/platform/${experiment.platform.toLowerCase().replaceAll(".", "")}/experiment/${experiment._id}`} color="inherit" underline="none"> */}
+                {experiment._id}
+                {/* </Link> */}
               </TableCell>
               <TableCell align="right">{experiment.name}</TableCell>
-              <TableCell align="right">{experiment.description || "N/A"}</TableCell>
+              <TableCell align="right">
+                {experiment.description || "N/A"}
+              </TableCell>
               <TableCell align="right">{experiment.platform}</TableCell>
               <TableCell align="right">
-                <IconButton aria-label="delete" className={classes.margin} size="small" onClick={handleClickDelete} expid={experiment._id}>
+                <IconButton
+                  aria-label="delete"
+                  className={classes.margin}
+                  size="small"
+                  onClick={handleClickDelete}
+                  expid={experiment._id}
+                >
                   <DeleteIcon fontSize="small" />
                 </IconButton>
               </TableCell>
@@ -104,7 +123,11 @@ function ExperimentTable({ expIds = [], delModP1, delModP2, handleDelete, snackO
         handleClose={handleDelModalClose}
         handleDelete={handleProcessDelete}
       />
-      <Snackbar open={snackOpen} autoHideDuration={3000} onClose={handleSnackClose}>
+      <Snackbar
+        open={snackOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackClose}
+      >
         <Alert onClose={handleSnackClose} severity={snackSev}>
           {snackMsg}
         </Alert>
